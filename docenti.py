1a7c7671506690a2f3d02866306d6317dba8c6c3
LorenzoSmedile <150134388+LorenzoSmedile@users.noreply.github.com>
2024-05-22 16:38:44 +0200

    Add files via upload

--- docenti.py
+++ docenti.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLineEdit ,QTableWidget,QTableWidgetItem,QHeaderView
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton ,QTableWidget,QTableWidgetItem
 from PyQt5 import uic
 import sys
  
@@ -10,19 +10,42 @@ class UI(QMainWindow):
         uic.loadUi(r"C:\Users\lorenzosmedile\Documents\docenti\mainwindow.ui", self)
         self.tableWidget = QTableWidget()
         
-        self.tableWidget.setColumnCount(3)  
-        self.tableWidget.setRowCount(3)
+
+        
+       
  
         # find the widgets in the xml file
  
         self.button= self.findChild(QPushButton,"caricaDocenti")
         self.button.clicked.connect(self.clickedBtn)
         self.show()
- 
+        
+        self.table = self.findChild(QTableWidget, "tabella")
+        self.table.setColumnCount(3)
+        self.table.setRowCount(3)
+        self.table.setHorizontalHeaderLabels(["Nome", "Cognome","Ore"])
+        
+        
+        
     
  
     def clickedBtn (self):
-        self.tableWidget.setItem(0,0, (str), "Ciao") 
+        lista= [{"nome":"pippo","cognome":"baudo","ore":"4"}]
+        lista2= [{"nome":"lorenzo","cognome":"smedile","ore":"10"}]
+        lista3= [{"nome":"matteo","cognome":"picciolini","ore":"6"}]
+        
+        
+        item_name = QTableWidgetItem("matteo")
+        self.table.setItem(0,0,item_name)
+        item_name = QTableWidgetItem("picciolini")
+        self.table.setItem(0,1,item_name)
+        item_name = QTableWidgetItem("18")
+        self.table.setItem(0,2,item_name)
+            
+        
+
+
+       
 
         
         
